63b6a72f4fcb4bc5b93ae0c69bab87787f3ef2f3
sen.huang <sen.huang@yeepay.com>
2018-07-05 11:44:44 +0800

    Executor的submit和execute的不同

--- src/main/java/com/thread/chapter5/ExecutorServiceTest.java
+++ src/main/java/com/thread/chapter5/ExecutorServiceTest.java
@@ -16,26 +16,33 @@ public class ExecutorServiceTest {
             public void run() {
                 try {
                     Thread.sleep(1000*30L);
-                    System.out.println("任务执行完成！！！");
+                    System.out.println("任务执行完成1！！！");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         };
-        threadPool.submit(r);
+        //submit 会等待提交的线程执行完成，execute不会
+//        threadPool.submit(r);
+        //
+        threadPool.execute(r);
+        System.out.println("线程1提交");
         Runnable r2 = new Runnable() {
             public void run() {
                 try {
                     Thread.sleep(1000*30L);
-                    System.out.println("任务执行完成！！！");
+                    System.out.println("任务执行完成2！！！");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         };
-        Future<Integer> submit = threadPool.submit(r2, 3);
-        System.out.println(submit.get());
+//        Future<Integer> submit = threadPool.submit(r2, 3);
+//        System.out.println(submit.get());
+        threadPool.execute(r2);
+        System.out.println("线程2提交");
         threadPool.shutdown();
+        System.out.println("有序关闭之前提交的线程");
         try {
             threadPool.awaitTermination(10, TimeUnit.HOURS);
         } catch (InterruptedException e) {
