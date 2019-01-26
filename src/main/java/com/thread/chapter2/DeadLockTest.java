3566cc221859ec6793d0cda06534093a6574fa41
sen.huang <sen.huang@yeepay.com>
2019-01-26 23:52:20 +0800

    死锁修改

--- src/main/java/com/thread/chapter2/DeadLockTest.java
+++ src/main/java/com/thread/chapter2/DeadLockTest.java
@@ -8,16 +8,22 @@ public class DeadLockTest {
     private static final Object lock1 = new Object();
     private static final Object lock2 = new Object();
 
-    public void invokeM1(){
+    public void invokeM1() throws Exception{
         synchronized (lock1){
+            //添加睡眠，等待invokeM2拿到lock2
+            Thread.sleep(50);
+            System.out.println("invokeM1 wake up");
             synchronized (lock2){
                 System.out.println("线程1执行");
             }
         }
     }
 
-    public void invokeM2(){
+    public void invokeM2() throws Exception{
         synchronized (lock2){
+            //添加睡眠，等待invokeM1拿到lock1
+            Thread.sleep(50);
+            System.out.println("invokeM2 wake up");
             synchronized (lock1){
                 System.out.println("线程2执行");
             }
@@ -28,30 +34,25 @@ public class DeadLockTest {
         final DeadLockTest test = new DeadLockTest();
         Runnable r1 = new Runnable() {
             public void run() {
-                while (true){
+                try {
                     test.invokeM1();
-                    try {
-                        Thread.sleep(50);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         };
 
         Runnable r2 = new Runnable() {
             public void run() {
-                while (true){
+                try {
                     test.invokeM2();
-                    try {
-                        Thread.sleep(50);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         };
-
+        System.out.println(lock1.toString());
+        System.out.println(lock2.toString());
         Thread t1 = new Thread(r1);
         Thread t2 = new Thread(r2);
         t1.start();
